c313b1e56ad58a195e6c3760662b327508b1c8e7
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-07-05 18:21:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+function setup() {
+    canvas = createCanvas(310, 310);
+    canvas.center();
+}
+
+function draw() {
+    
+}
+
+function snap() {
+    save('MyMustache-Image.png')
+}
